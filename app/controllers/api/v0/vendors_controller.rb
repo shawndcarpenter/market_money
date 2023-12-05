21c19e7e805bbc7381af4d94d60c5010c54adae8
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-04 18:21:08 -0700

    Set up vendor route and controller with commented out error handling

---
+++ app/controllers/api/v0/vendors_controller.rb
@@ -0,0 +1,15 @@
+class Api::V0::VendorsController < ApplicationController
+  # rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
+  def index
+    market = Market.find(params[:market_id])
+    vendors = market.vendors
+    render json: VendorSerializer.new(vendors)
+  end
+
+  # private
+  # def not_found_response(exception)
+  #   render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+  #   .serialize_json, status: :not_found
+  # end
+end
