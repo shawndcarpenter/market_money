086a0aca887c966e2a2d3b4ab7c4ab6996a49b2a
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-04 18:42:05 -0700

    Add error handling for market vendors endpoint

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,5 +1,5 @@
 class Api::V0::VendorsController < ApplicationController
-  # rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
 
   def index
     market = Market.find(params[:market_id])
@@ -7,9 +7,9 @@ class Api::V0::VendorsController < ApplicationController
     render json: VendorSerializer.new(vendors)
   end
 
-  # private
-  # def not_found_response(exception)
-  #   render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
-  #   .serialize_json, status: :not_found
-  # end
+  private
+  def not_found_response(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+    .serialize_json, status: :not_found
+  end
 end
