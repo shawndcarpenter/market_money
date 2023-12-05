56d39a2cb3cdd1aaa7f03b8a146d9059449938dd
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-04 17:36:39 -0700

    Write tests for market show endpoint

--- spec/requests/api/v0/markets_request_spec.rb
+++ spec/requests/api/v0/markets_request_spec.rb
@@ -51,4 +51,63 @@ describe "Markets API" do
       expect(market[:attributes][:vendor_count]).to be_a(Integer)
     end
   end
+
+  it "can get one market by its id" do
+    id = create(:market).id
+
+    get "/api/v0/markets/#{id}"
+
+    market = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(response).to be_successful
+
+    expect(market).to have_key(:type)
+    expect(market[:type]).to be_a(String)
+    expect(market[:type]).to eq("market")
+    
+    expect(market).to have_key(:attributes)
+    expect(market[:attributes]).to be_an(Hash)
+     
+    expect(market[:attributes]).to have_key(:name)
+    expect(market[:attributes][:name]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:street)
+    expect(market[:attributes][:street]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:city)
+    expect(market[:attributes][:city]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:county)
+    expect(market[:attributes][:county]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:state)
+    expect(market[:attributes][:state]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:zip)
+    expect(market[:attributes][:zip]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:lat)
+    expect(market[:attributes][:lat]).to be_a(String)
+
+    expect(market[:attributes]).to have_key(:lon)
+    expect(market[:attributes][:lon]).to be_a(String)
+      
+    expect(market[:attributes]).to have_key(:vendor_count)
+    expect(market[:attributes][:vendor_count]).to be_a(Integer)
+  end
+
+  describe "sad paths" do
+    it "will gracefully handle if a market id doesn't exist" do
+      get "/api/v0/markets/1"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:title]).to eq("Couldn't find Market with 'id'=1")
+    end
+  end
 end
