28d9bc90eb52e9bbfef9792650e796a73ba4d008
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-04 18:38:36 -0700

    Write remainder of happy path test for market vendors endpoint

--- spec/factories/vendors.rb
+++ spec/factories/vendors.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :vendor do
     name { Faker::TvShows::TwinPeaks.location }
     description { Faker::TvShows::DrWho.quote }
-    contact_name { Faker::TvShows::DrWho.character }
+    contact_name { Faker::FunnyName.two_word_name }
     contact_phone { Faker::Number.number(digits: 10) }
     credit_accepted { Faker::Boolean.boolean }
   end
--- spec/requests/api/v0/market_vendors_request_spec.rb
+++ spec/requests/api/v0/market_vendors_request_spec.rb
@@ -11,5 +11,34 @@ describe "Market Vendors" do
     expect(response).to be_successful
 
     vendors = JSON.parse(response.body, symbolize_names: true)
+
+    expect(vendors[:data].count).to eq(5)
+
+    vendors[:data].each do |vendor|
+      # expect(vendor).to have_key(:id)
+      # expect(vendor[:id]).to eq(id)
+
+      expect(vendor).to have_key(:type)
+      expect(vendor[:type]).to be_a(String)
+      expect(vendor[:type]).to eq("vendor")
+     
+      expect(vendor).to have_key(:attributes)
+      expect(vendor[:attributes]).to be_an(Hash)
+     
+      expect(vendor[:attributes]).to have_key(:name)
+      expect(vendor[:attributes][:name]).to be_a(String)
+     
+      expect(vendor[:attributes]).to have_key(:description)
+      expect(vendor[:attributes][:description]).to be_a(String)
+     
+      expect(vendor[:attributes]).to have_key(:contact_name)
+      expect(vendor[:attributes][:contact_name]).to be_a(String)
+     
+      expect(vendor[:attributes]).to have_key(:contact_phone)
+      expect(vendor[:attributes][:contact_phone]).to be_a(String)
+     
+      expect(vendor[:attributes]).to have_key(:credit_accepted)
+      expect(vendor[:attributes][:credit_accepted]).to be_a(TrueClass).or be_a(FalseClass)
+    end
   end
 end
