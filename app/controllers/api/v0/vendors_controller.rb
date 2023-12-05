d32552478906f7afb4a23042cdcd439e00b160ac
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-05 15:53:31 -0700

    Write functionality for endpoint 5, vendor create

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -1,5 +1,6 @@
 class Api::V0::VendorsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_response
 
   def index
     market = Market.find(params[:market_id])
@@ -11,9 +12,23 @@ class Api::V0::VendorsController < ApplicationController
     render json: VendorSerializer.new(Vendor.find(params[:id]))
   end
 
+  def create
+    render json: VendorSerializer.new(Vendor.create!(vendor_params))
+  end
+
   private
   def not_found_response(exception)
     render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
     .serialize_json, status: :not_found
+  end
+
+  def invalid_response(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 422))
+    .serialize_json, status: :not_found
+  end
+
+
+  def vendor_params
+    params.require(:vendor).permit(:name, :description, :contact_name, :contact_phone, :credit_accepted)
   end
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,9 @@
 class Vendor < ApplicationRecord
   has_many :market_vendors
   has_many :markets, through: :market_vendors
+  validates_presence_of :name
+  validates_presence_of :description
+  validates_presence_of :contact_name
+  validates_presence_of :contact_phone
+  validates_presence_of :credit_accepted
 end
