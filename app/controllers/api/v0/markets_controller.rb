4cb382ecda3304837d62773c512a4d852ba57621
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-04 17:37:22 -0700

    Create show method and error handling methods

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,6 +1,19 @@
 class Api::V0::MarketsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found_response
+
   def index
     markets = Market.all
     render json: MarketSerializer.new(markets)
+  end
+  
+  def show
+    render json: 
+    MarketSerializer.new(Market.find(params[:id]))
+  end
+
+  private
+  def not_found_response(exception)
+    render json: ErrorSerializer.new(ErrorMessage.new(exception.message, 404))
+    .serialize_json, status: :not_found
   end
 end
