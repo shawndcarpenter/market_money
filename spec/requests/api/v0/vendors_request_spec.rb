98a52e869bc19c233c45fad006c0403c1a57c878
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-04 18:55:23 -0700

    Add testing for endpoint 4 sad path

--- spec/requests/api/v0/vendors_request_spec.rb
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -36,4 +36,19 @@ describe "Vendor API Request" do
     expect(vendor[:attributes]).to have_key(:credit_accepted)
     expect(vendor[:attributes][:credit_accepted]).to be_a(TrueClass).or be_a(FalseClass)
   end
+
+  describe "sad paths" do
+    it "will gracefully handle if a vendor id doesn't exist" do
+      get "/api/v0/vendors/123123123123"
+
+      expect(response).to_not be_successful
+      expect(response.status).to eq(404)
+
+      data = JSON.parse(response.body, symbolize_names: true)
+
+      expect(data[:errors]).to be_a(Array)
+      expect(data[:errors].first[:status]).to eq("404")
+      expect(data[:errors].first[:title]).to eq("Couldn't find Vendor with 'id'=123123123123")
+    end
+  end
 end
