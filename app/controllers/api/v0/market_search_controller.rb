34f9c3e7be9a38dfcc070ac4e8ce6ef43fa760c2
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-07 17:24:08 -0700

    refactor market search controller

--- app/controllers/api/v0/market_search_controller.rb
+++ app/controllers/api/v0/market_search_controller.rb
@@ -1,5 +1,19 @@
 class Api::V0::MarketSearchController < ApplicationController
   def index
+    raise InvalidSearch unless is_valid?(params)
+    find_matching_market(params)
+  end
+
+  private
+  def is_valid?(params)
+    (params[:state].present? && params[:city].present? && params[:name].present?) ||
+    (params[:state].present? && params[:city].present?) ||
+    (params[:state].present? && params[:name].present?) ||
+    (params[:state].present?) ||
+    (params[:name].present? && !params[:city].present?)
+  end
+
+  def find_matching_market(params)
     if params[:state].present? && params[:city].present? && params[:name].present?
       searched_markets = Market.search_by_city_and_state_and_name(params[:city], params[:state], params[:name])
       render json: MarketSerializer.new(searched_markets)
@@ -15,14 +29,6 @@ class Api::V0::MarketSearchController < ApplicationController
     elsif params[:name].present? && !params[:city].present?
       searched_markets = Market.search_by_name(params[:name])
       render json: MarketSerializer.new(searched_markets)
-    else
-      render json: {
-        "errors": [
-            {
-                "detail": "Invalid set of parameters. Please provide a valid set of parameters to perform a search with this endpoint."
-            }
-        ]
-      }, status: 422
     end
   end
 end
