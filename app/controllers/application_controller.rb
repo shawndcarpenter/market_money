b00e0191ae5095dbb5656fab4b21e636cdebc047
Shawn Carpenter <shawncarpenter.co@gmail.com>
2023-12-07 20:11:13 -0700

    Refactor error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   class InvalidSearch < StandardError
   end
 
-rescue_from InvalidSearch, :with => :invalid_search_response
+  rescue_from InvalidSearch, :with => :invalid_search_response
 
   def invalid_search_response(exception)
     render json: ErrorSerializer.new(ErrorMessage.new("Invalid set of parameters. Please provide a valid set of parameters to perform a search with this endpoint.", status: 422)).serialize_json, status: 422
